370bdebf5d1469c619be08378cac3ea78954f6c4
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2025-06-30 14:21:06 -0400

    Update build_star_index.sh

--- build_star_index.sh
+++ build_star_index.sh
@@ -1,20 +1,24 @@
-#!/bin/bash
 #SBATCH --job-name=star_index
 #SBATCH --nodes=1
 #SBATCH --ntasks=8
-#SBATCH --mem=60G            # <-- increase RAM significantly (e.g., 60GB)
+#SBATCH --mem=60G
 #SBATCH --time=02:00:00
 #SBATCH --output=star_index.log
+#SBATCH --error=star_index.err
 
+# Activate STAR environment
+conda activate sra_download_env
 
-cd /work/pi_mohammadariful_alam_uml_edu
-mkdir -p nazim_macca_ref
-cd nazim_macca_ref
+# Set working directory for the index
+index_dir="/scratch/workspace/nazimahmed_belabbaci_student_uml_edu-SplicingWorkspace/nazim_SARS_Covid/STARindex"
+mkdir -p "$index_dir"
 
-STAR \
-  --runThreadN 8 \
+# Run STAR index generation
+STAR --runThreadN 8 \
   --runMode genomeGenerate \
-  --genomeDir STARindex \
+  --genomeDir "$index_dir" \
   --genomeFastaFiles /home/nazimahmed_belabbaci_student_uml_edu/macca_ref/Macaca_mulatta.Mmul_10.dna.toplevel.fa \
   --sjdbGTFfile /home/nazimahmed_belabbaci_student_uml_edu/macca_ref/Macaca_mulatta.Mmul_10.108.gtf \
-  --sjdbOverhang 74
+  --sjdbOverhang 100 \
+  --limitGenomeGenerateRAM 50000000000 \
+  --genomeSAsparseD 2
